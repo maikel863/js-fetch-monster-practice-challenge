5b85688bd48346c7e19918b736c94597118c1327
Maikel <maikel863@outlook.com>
2021-04-17 15:02:17 -0400

    Done with assignment

---
+++ js/index.js
@@ -0,0 +1,167 @@
+const BASE_URL = "http://localhost:3000";
+
+
+// Get relevant data
+function getMonsters(page) {
+    const url = `${BASE_URL}/monsters/?_limit=50&_page=${page}`
+    fetch(url)
+        .then(r => r.json())
+        .then(data => console.log(data));
+}
+
+// Create component with data
+/* <div>
+    <h2>name</h2>
+    <h3>age</h3>
+    <p>description</p>
+</div> */
+
+function createMonsterDiv(monsterObj) {
+    const div = document.createElement('div'),
+        h2 = document.createElement('h2'),
+        h3 = document.createElement('h3'),
+        p = document.createElement('p');
+
+    h2.textContent = monsterObj.name;
+    h3.textContent = monsterObj.age;
+    p.textContent = monsterObj.description;
+
+    div.appendChild(h2);
+    div.appendChild(h3);
+    div.appendChild(p);
+
+    return div;
+}
+
+// Append the component to the DOM
+
+function showMonsters(monstersArray) {
+    const monsterContainer = document.getElementById('monster-container');
+    monstersArray.forEach(monsterObj => {
+        const monsterDiv = createMonsterDiv(monsterObj);
+        monsterContainer.appendChild(monsterDiv);
+    })
+}
+
+function clearMonstersContainer() {
+    document.querySelector("#monster-container").innerHTML = "";
+  }
+  
+  const testMonstersArray = [
+    {
+      "name": "Chronos",
+      "age": 4005.302453418598,
+      "description": "Effulgence eldritch shunned foetid. Ululate gibbering tenebrous foetid iridescence daemoniac. Stench nameless gambrel. Amorphous furtive iridescence noisome. Foetid mortal nameless.",
+      "id": 1
+    },
+    {
+      "name": "Tartarus",
+      "age": 1874.4913565609456,
+      "description": "Cyclopean swarthy amorphous singular accursed furtive non-euclidean stygian. Swarthy gibbering charnel eldritch daemoniac gibbous. Cyclopean lurk hideous tentacles squamous immemorial tenebrous mortal. Madness tentacles furtive mortal foetid decadent. Foetid immemorial comprehension.",
+      "id": 2
+    },
+    {
+      "name": "Hemera",
+      "age": 4094.8375978925988,
+      "description": "Dank immemorial abnormal gambrel. Cat lurk unutterable. Abnormal tenebrous ululate. Nameless swarthy manuscript eldritch indescribable accursed antediluvian decadent.",
+      "id": 3
+    },
+    {
+      "name": "Tartarus",
+      "age": 2364.163894640274,
+      "description": "Fungus blasphemous immemorial indescribable singular cat foetid. Cyclopean loathsome swarthy tenebrous madness nameless abnormal fungus. Daemoniac fungus fainted.",
+      "id": 4
+    }
+  ]
+
+// OBJECTIVE 2: Form for creating new monsters
+function createMonsterForm() {
+    const form = document.createElement("form"),
+      nameInput = document.createElement("input"),
+      ageInput = document.createElement("input"),
+      descInput = document.createElement("input"),
+      submitBtn = document.createElement("button");
+  
+    form.id = "monster-form";
+    nameInput.id = "name";
+    ageInput.id = "age";
+    descInput.id = "description";
+  
+    nameInput.placeholder = "name";
+    ageInput.placeholder = "age";
+    descInput.placeholder = "description";
+    submitBtn.textContent = "Create";
+  
+    form.appendChild(nameInput);
+    form.appendChild(ageInput);
+    form.appendChild(descInput);
+    form.appendChild(submitBtn);
+  
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const monsterObj = getFormData();
+      postMonster(monsterObj);
+      clearForm();
+      });
+  
+    document.getElementById("create-monster").appendChild(form);
+  }
+  
+  function getFormData() {
+    let a = document.querySelector("#name"),
+      b = document.querySelector("#age"),
+      c = document.querySelector("#description");
+  
+    return {
+      name: a.value,
+      age: parseFloat(b.value),
+      description: c.value,
+    };
+  }
+  
+  function postMonster(monster) {
+    let URL = `${BASE_URL}monsters`,
+      config = {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify(monster),
+      };
+  
+    fetch(URL, config);
+  }
+  
+  function clearForm() {
+    document.querySelector("#monster-form").reset();
+  }
+  
+// OBJECTIVE 3: Control page using the buttons
+
+function addNavListeners() {
+    let backBtn = document.querySelector("#back"),
+      forwardBtn = document.querySelector("#forward");
+  
+    backBtn.addEventListener("click", () => {
+      prevPage();
+    });
+  
+    forwardBtn.addEventListener("click", () => {
+      nextPage();
+    });
+  }
+  
+  function nextPage() {
+    curPage++;
+    getMonsters(curPage).then(showMonsters);
+  }
+  
+  function prevPage() {
+    if (curPage < 1) {
+      alert("You're already on the first page");
+    } else {
+      curPage--;
+      getMonsters(curPage).then(showMonsters);
+    }
+  }  
